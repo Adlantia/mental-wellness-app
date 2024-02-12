a917305a42087693404d38bdda390866c1e8a3f0
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-02-12 16:11:57 -0700

    Fixed dropdown to be visible

--- frontend/src/app/Navigation.tsx
+++ frontend/src/app/Navigation.tsx
@@ -1,12 +1,13 @@
+import { metadata} from "@/app/layout";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 export function Navigation () {
     return (
-        <div className="navbar bg-neutral justify-between">
+        <div className="navbar bg-base-300 justify-between">
             <div className="flex-none">
                 <button className="btn btn-circle btn-ghost color-primary">
-                    <FontAwesomeIcon className="w-5 h-5" icon={faHouse} />
+                    <a><FontAwesomeIcon className="w-5 h-5" icon={faHouse}/></a>
                 </button>
             </div>
             <div>
@@ -19,29 +20,22 @@ export function Navigation () {
                              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
                     </div>
                 </div>
-                <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                         className="inline-block w-5 h-5 stroke-current">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                              d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </div>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                     className="inline-block w-5 h-5 stroke-current">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                          d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </div>
                     <ul tabIndex={0}
-                        className="menu menu-sm dropdown-content mx-auto z-[1] shadow bg-neutral rounded-box w-52">
+                        className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-300 rounded-box w-52">
                         <li><a>Mood</a></li>
                         <li><a>Sleep</a></li>
                         <li><a>Journal</a></li>
-                        <li><a>Sign Out</a></li>
+                        <li><a>Logout</a></li>
                     </ul>
                 </div>
-                {/*<button className="btn btn-circle btn-ghost">*/}
-                {/*    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"*/}
-                {/*         className="inline-block w-5 h-5 stroke-current">*/}
-                {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"*/}
-                {/*              d="M4 6h16M4 12h16M4 18h16"></path>*/}
-                {/*    </svg>*/}
-                {/*</button>*/}
             </div>
         </div>
     )
