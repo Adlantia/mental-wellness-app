416f0a1cd1b5f65de1de5fb4bf12dff00e167189
Ashish Khatri <akhatri1@cnm.edu>
2024-02-12 15:23:31 -0700

    added files for commiting

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export function Dashboard () {
+    return (
+    <>
+
+        <html data-theme="dark">
+        <div data-theme="light">
+            Dashboard board
+            <span data-theme="retro">This span will always use retro theme!</span>
+        </div>
+
+        <body>
+
+        </body>
+        </html>
+    </>
+    )
+}
