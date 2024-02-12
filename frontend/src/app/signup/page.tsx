f14f65980aeff5b2e5c5c472fe43d852c4331329
Adelita Martinez <amartinez1013@cnm.edu>
2024-02-12 16:31:14 -0700

    Centered signup form

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -2,25 +2,26 @@
 export default function Signup() {
    return (
        <>
+           <div className="container flex justify-center items-center h-screen">
+           <form className="max-w-96 justify-center grid grid-rows-4">
+               <h1 className="font-bold text-xl text-center pt-3"> App Name </h1>
 
-       <h1 className="font-bold text-xl text-center py-8"> App Name </h1>
-       <form className="items-center text-center px-6 grid grid-rows-4">
-           <h1 className="text-lg text-center pt-2">Sign Up</h1>
-           <p className="text-center py-1">Please fill in this form to create an account.</p>
+               <h1 className="text-lg text-center pt-1">Sign Up</h1>
+               <p className="text-center ">Please fill in this form to create an account.</p>
 
-           <label className="py-3" htmlFor="email">Email: </label>
-           <input type="email" placeholder="Enter Email" name="email" required/>
+               <label className="py-2 my-3" htmlFor="email">Email: </label>
+               <input className="py-2" type="email" placeholder="Enter Email" name="email" required/>
 
-           <label className="py-3" htmlFor="password">Password: </label>
-           <input type="password" placeholder="Enter New Password" name="password" required/>
+               <label className="py-3 my-3" htmlFor="password">Password: </label>
+               <input className="py-1" type="password" placeholder="Enter New Password" name="password" required/>
 
-           <label className="py-3" htmlFor="password">Re-enter Password: </label>
-           <input type="password" placeholder="Re-enter Password" name="password" required></input>
+               <label className="py-3 my-3" htmlFor="password">Re-enter Password: </label>
+               <input className="py-1" type="password" placeholder="Re-enter Password" name="password" required></input>
 
-           <button className="justify-items-center font-bold py-2 my-6 bg-slate-600 rounded-full">Sign Up</button>
+               <button className="font-bold py-2 my-6 bg-slate-600 rounded-full w-full">Sign Up</button>
 
-
-       </form>
+           </form>
+           </div>
 
 
        </>
