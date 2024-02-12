df7359263b7262cff8aad4ad379b100aee8721fd
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-02-12 15:29:19 -0700

    Add dropdown menu

--- frontend/src/app/Navigation.tsx
+++ frontend/src/app/Navigation.tsx
@@ -5,7 +5,7 @@ export function Navigation () {
     return (
         <div className="navbar bg-neutral justify-between">
             <div className="flex-none">
-                <button className="btn btn-square btn-ghost color-primary">
+                <button className="btn btn-circle btn-ghost color-primary">
                     <FontAwesomeIcon className="w-5 h-5" icon={faHouse} />
                 </button>
             </div>
@@ -19,13 +19,29 @@ export function Navigation () {
                              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"/>
                     </div>
                 </div>
-                <button className="btn btn-square btn-ghost">
+                <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                          className="inline-block w-5 h-5 stroke-current">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                               d="M4 6h16M4 12h16M4 18h16"></path>
                     </svg>
-                </button>
+                </div>
+                    <ul tabIndex={0}
+                        className="menu menu-sm dropdown-content mx-auto z-[1] shadow bg-neutral rounded-box w-52">
+                        <li><a>Mood</a></li>
+                        <li><a>Sleep</a></li>
+                        <li><a>Journal</a></li>
+                        <li><a>Sign Out</a></li>
+                    </ul>
+                </div>
+                {/*<button className="btn btn-circle btn-ghost">*/}
+                {/*    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"*/}
+                {/*         className="inline-block w-5 h-5 stroke-current">*/}
+                {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"*/}
+                {/*              d="M4 6h16M4 12h16M4 18h16"></path>*/}
+                {/*    </svg>*/}
+                {/*</button>*/}
             </div>
         </div>
     )
