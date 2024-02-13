9e5ee8d398e4d54ccf758864123761c65515ce03
Adelita Martinez <amartinez1013@cnm.edu>
2024-02-13 15:48:50 -0700

    Added sign in (unfinished)

---
+++ frontend/src/app/sign-in/signin-page.tsx
@@ -0,0 +1,7 @@
+export default function Signin() {
+    return(
+        <>
+        <h1>Hello, testing123</h1>
+        </>
+    )
+}
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -9,16 +9,16 @@ export default function Signup() {
                <h1 className="text-lg text-center pt-1">Sign Up</h1>
                <p className="text-center ">Please fill in this form to create an account.</p>
 
-               <label className="py-2 my-3" htmlFor="email">Email: </label>
-               <input className="py-2" type="email" placeholder="Enter Email" name="email" required/>
+               <label className="py-3 my-3" htmlFor="email">Email: </label>
+               <input className="input input-bordered input-sm w-full max-w-xs" type="email" placeholder="Enter Email" name="email" required/>
 
                <label className="py-3 my-3" htmlFor="password">Password: </label>
-               <input className="py-1" type="password" placeholder="Enter New Password" name="password" required/>
+               <input className="input input-bordered input-sm w-full max-w-xs" type="password" placeholder="Enter New Password" name="password" required/>
 
                <label className="py-3 my-3" htmlFor="password">Re-enter Password: </label>
-               <input className="py-1" type="password" placeholder="Re-enter Password" name="password" required></input>
+               <input className="input input-bordered input-sm w-full max-w-xs" type="password" placeholder="Re-enter Password" name="password" required></input>
 
-               <button className="font-bold py-2 my-6 bg-slate-600 rounded-full w-full">Sign Up</button>
+               <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg w-full md:w-auto mt-4">Sign Up</button>
 
            </form>
            </div>
