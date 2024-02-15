a97a47aafae2cef21b3f9ab589f0727023685264
Adelita Martinez <amartinez1013@cnm.edu>
2024-02-15 10:45:54 -0700

    Updated sign-in button

--- frontend/src/app/sign-in/page.tsx
+++ frontend/src/app/sign-in/page.tsx
@@ -1,28 +1,40 @@
+import React from "react";
 
 
 export default function Signin() {
     return (
         <>
-            <div className="container flex justify-center">
-                <form className="max-w-96 justify-center grid grid-rows-2">
-                    <h1 className="font-bold text-xl text-center mt-3"> Sign In</h1>
-                    <p className="text-center mt-3">Please log in using the form below, or press "Create account"</p>
-                    
-                     <label className="py-3 my-3" htmlFor="email">Email: </label>
-                     <input className="input input-bordered input-sm w-full max-w-xs" type="email"
-                           placeholder="Enter Email" name="email" required/>
+            <div className="flex justify-center items-center h-screen">
+                <div className="w-96 p-6 shadow-lg bg-white rounded-md">
+                    <h1 className="text-3xl block text-center font-semi-bold"><i className="fa-solid fa-user"></i> Login</h1>
+                    <hr className="mt-3"></hr>
+                    <div className="mt-3">
+                        <label htmlFor="email" className="block text-base mb-2">Email</label>
+                        <input type="email" id="email"
+                               className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+                               placeholder="Enter Email..." required/>
+                    </div>
+                    <div className="mt-3">
+                        <label htmlFor="password" className="block text-base mb-2">Password</label>
+                        <input type="text" id="password"
+                               className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
+                               placeholder="Enter Password..." required/>
+                    </div>
+                    <div className="mt-3 flex justify-between items-center">
+                        <div>
+                            <input type="checkbox"/>
+                            <label htmlFor="checkbox">Remember Me</label>
+                        </div>
+                         <div>
+                            <a href="#" className="font-semi-bold text-indigo-900">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div className="mt-5">
+                        <button type="submit" className="border-2 border-indigo-700 bg-indigo-700 text-white py-1 w-full rounded-md hover:bg-transparent hover:text-indigo-700 font-semibold">Login</button>
 
-                     <label className="py-3 my-3" htmlFor="password">Password: </label>
-                     <input className="input input-bordered input-sm w-full max-w-xs" type="password"
-                           placeholder="Enter Password" name="password" required/>
+                    </div>
 
-
-                    <button className="btn btn-sm w-full my-4">Sign In</button>
-
-                    <button className="btn btn-sm w-full my-4">Create account
-                    </button>
-
-                </form>
+                </div>
             </div>
 
 
