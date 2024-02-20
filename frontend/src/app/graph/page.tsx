0469f3eb2461f197059cda4cabc4e3705e3524e7
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-02-20 07:52:08 -0700

    Add linechart

--- frontend/src/app/graph/page.tsx
+++ frontend/src/app/graph/page.tsx
@@ -1,13 +1,36 @@
 
 'use client'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}];
+import {LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts';
+
+
+
+const data = [
+    {
+        date: 'test', sleep: 3, mood: 5
+    },
+    {
+        date: 'test', sleep: 5, mood: 2
+    },
+    {
+        date: 'test', sleep: 1, mood: null
+    },
+    {
+        date: 'test', sleep: 3, mood: 3
+    }
+];
+
+for (let i = 0; i < data.length; i++) {
+    if ( i !== 0 && i !== data.length - 1 && data[i].mood === null) {
+        data[i].mood = (data[i - 1].mood + data[i + 1].mood) / 2
+    }
+}
 
 const renderLineChart = (
   <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+    <Line type="monotone" dataKey="mood" stroke="#8884d8" />
+      <Line type="monotone" dataKey="sleep" stroke="#FFFFFF" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="name" />
+    <XAxis dataKey="date" />
     <YAxis />
     <Tooltip />
   </LineChart>
