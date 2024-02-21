7bac5e0a12267ceeaeb01843d7a3eedeca889599
DMCatanach <dawn.catanach@gmail.com>
2024-02-21 14:29:47 -0700

    Dashboard page and navbar now link to other pages

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -4,9 +4,9 @@ export default function Dashboard () {
     return (
         <>
             <section className="container mx-auto py-12">
-                <LogButton buttonName = 'Mood'/>
-                <LogButton buttonName = 'Sleep'/>
-                <LogButton buttonName = 'Journal'/>
+                <LogButton buttonName = <a href='/mood'>'Mood'</a> />
+                <LogButton buttonName = <a href='/sleep'>'Sleep'</a> />
+                <LogButton buttonName = <a href='/journal-list'>'Journal'</a> />
                 </section>
 
         </>
