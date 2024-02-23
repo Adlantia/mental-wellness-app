d912349a678c5f063f8e79be0add53b5322b54cd
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-02-23 08:52:56 -0700

    Adjust graph styles

--- frontend/src/app/graph/page.tsx
+++ frontend/src/app/graph/page.tsx
@@ -6,10 +6,10 @@ import {LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts';
 
 const data = [
     {
-        date: 'test', sleep: 3, mood: 5
+        date: '2/21/24', sleep: 3, mood: 5
     },
     {
-        date: 'test', sleep: 5, mood: 2
+        date: 'test', sleep: null, mood: 2
     },
     {
         date: 'test', sleep: 1, mood: null
@@ -19,16 +19,16 @@ const data = [
     }
 ];
 
-for (let i = 0; i < data.length; i++) {
-    if ( i !== 0 && i !== data.length - 1 && data[i].mood === null) {
-        data[i].mood = (data[i - 1].mood + data[i + 1].mood) / 2
-    }
-}
+// for (let i = 0; i < data.length; i++) {
+//     if ( i !== 0 && i !== data.length - 1 && data[i].mood === null) {
+//         data[i].mood = (data[i - 1].mood + data[i + 1].mood) / 2
+//     }
+// }
 
 const renderLineChart = (
   <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-    <Line type="monotone" dataKey="mood" stroke="#8884d8" />
-      <Line type="monotone" dataKey="sleep" stroke="#FFFFFF" />
+    <Line connectNulls type="monotone" dataKey="mood" stroke="#8884d8" />
+      <Line connectNulls type="monotone" dataKey="sleep" stroke="#FF0000" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
     <XAxis dataKey="date" />
     <YAxis />
@@ -37,7 +37,7 @@ const renderLineChart = (
 );
 export default function Graph () {
     return (
-        <div>
+        <div className="mx-auto flex">
             <h1>Graph</h1>
             {renderLineChart}
         </div>
