c7254e0f484cdc6bbadebddd179ada9e37785f5f
Lex Garey-Fernandez <lexgarey@gmail.com>
2024-02-23 09:58:53 -0700

    fix styles

--- frontend/src/app/graph/page.tsx
+++ frontend/src/app/graph/page.tsx
@@ -1,6 +1,6 @@
 
 'use client'
-import {LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis} from 'recharts';
+import {LineChart, Line, CartesianGrid, Tooltip, YAxis} from 'recharts';
 
 
 
@@ -19,27 +19,25 @@ const data = [
     }
 ];
 
-// for (let i = 0; i < data.length; i++) {
-//     if ( i !== 0 && i !== data.length - 1 && data[i].mood === null) {
-//         data[i].mood = (data[i - 1].mood + data[i + 1].mood) / 2
-//     }
-// }
-
 const renderLineChart = (
   <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
     <Line connectNulls type="monotone" dataKey="mood" stroke="#8884d8" />
       <Line connectNulls type="monotone" dataKey="sleep" stroke="#FF0000" />
     <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-    <XAxis dataKey="date" />
     <YAxis />
     <Tooltip />
   </LineChart>
 );
 export default function Graph () {
     return (
-        <div className="mx-auto flex">
-            <h1>Graph</h1>
-            {renderLineChart}
+        <div className="mx-auto flex flex-col">
+            <div className="container">
+            <h1 className="
+">Graph</h1>
+            <div className="">
+                {renderLineChart}
+            </div>
+            </div>
         </div>
     )
 }
