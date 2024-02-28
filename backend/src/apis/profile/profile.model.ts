3c5536bf67ff17415e492018ee4e5d49e60fb233
Ashish Khatri <akhatri1@cnm.edu>
2024-02-28 13:18:07 -0700

    added accountValidator files for email confirmation

--- backend/src/apis/profile/profile.model.ts
+++ backend/src/apis/profile/profile.model.ts
@@ -24,5 +24,25 @@ export async function insertProfile (profile: PrivateProfile): Promise<string> {
     return 'Profile Successfully Created'
 }
 
+/**
+ * updates a profile in the profile table
+ * @param profile
+ * @returns {Promise<string>} 'Profile successfully updated'
+ */
+export async function updateProfile (profile: PrivateProfile): Promise <string> {
+    const {profileId, profileActivationToken, profileEmail, profileHash, profileName} = profile
+    await sql `update profile set profile_activation_token = ${profileActivationToken}, profile_email = ${profileEmail}, profile_hash = ${profileHash}, profile_name = ${profileName} where profile_id = ${profileId}`
+    return 'Profile successfully updated'
+}
+/**
+ * selects a profile from the profile table by profileActivationToken
+ * @param profileActivationToken the profile's activation token to search for in the profile table
+ * @return profile or null if no profile was found
+ */
+export async function selectPrivateProfileByProfileActivationToken (profileActivationToken: string): Promise<PrivateProfile|null> {
+    const rowList =  await sql `select profile_id, profile_activation_token, profile_email, profile_hash, profile_name from profile where profile_activation_token = ${profileActivationToken}`
+    const result = PrivateProfileSchema.array().max(1).parse(rowList)
+    return result?.length === 1 ? result[0] : null
+}
 
 
