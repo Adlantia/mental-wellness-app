609c934651b65626c6a2a317f4a1a4055185e343
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-04 13:25:30 -0700

    Modified tracker.controller

--- backend/src/apis/tracker/tracker.controller.ts
+++ backend/src/apis/tracker/tracker.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import {TrackerSchema} from "./tracker.validator";
 import {zodErrorResponse} from "../../utils/response.utils";
 import {PrivateProfile} from "../profile/profile.model";
+import {insertTracker} from "./tracker.model";
+import {Status} from "../../utils/interfaces/Status";
 
 
 export async function postTrackerController(request: Request, response: Response): Promise<Response | undefined> {
@@ -25,5 +27,16 @@ export async function postTrackerController(request: Request, response: Response
         const trackerProfileId: string = profile.profileId as string
 
         // insert the tracker into the database and store the result in a variable called result
+        const result = await insertTracker(tracker)
+
+        // return the response with the status code 200, a message, and the result as data
+        const status: Status = {status: 200, message: result, data: null}
+        return response.json(status)
+
+        // if there is an error,  return the response with the status 500, an error message, and null data
+    } catch (error) {
+        console.log(error)
+        return response.json({status:500, message: 'Error creating tracker. Try again.', data: null})
     }
 }
+
