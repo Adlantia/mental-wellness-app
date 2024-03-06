9515be9c3e8623522ccd992c9745722c9f818721
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-06 15:19:21 -0700

    Fixed error

--- backend/src/apis/sign-out/sign-out.route.ts
+++ backend/src/apis/sign-out/sign-out.route.ts
@@ -7,7 +7,8 @@ const basePath = '/api/sign-out'
 const router = Router()
 
 // define sign-out route for this router
-router.route('/').get(signOutController)
+router.route('/')
+    .get(signOutController)
 
 // export the router with the basePath and router object
 export const signOutRoute = { basePath, router }
