64f6637586baf4e69ba4e40c78d630070e5d2804
DMCatanach <dawn.catanach@gmail.com>
2024-03-08 08:49:28 -0700

    Completes seed data

--- seed-data/seedData.js
+++ seed-data/seedData.js
@@ -13,152 +13,152 @@ let trackerData = [
 
 let logData = [
     {
-       "logId": "",
+       "logId": "20f013f3-e541-4a7f-936f-f5b224a63c76",
        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
        "logAnswer": 4,
-       "logDatetime": "" 
+       "logDatetime": "2024-01-22T09:00:00Z" 
     },
     {
-        "logId": "",
+        "logId": "8ece0cfe-7e89-4e25-a3e2-ae4b67c56970",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 2,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-22T09:05:00Z" 
      },
      {
-        "logId": "",
+        "logId": "8de17e3a-b7b2-4b81-8516-67249173df6b",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 2,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-23T10:00:00Z" 
      },
      {
-        "logId": "",
+        "logId": "75b549c2-9a90-466a-9114-8f0355bc926a",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 5,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-23T10:00:10Z" 
      },
      {
-        "logId": "",
+        "logId": "a3efcf4a-525d-400c-ad37-324ece7f37ec",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 2,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-24T09:45:00Z" 
      },
      {
-        "logId": "",
+        "logId": "c631efc6-9438-4c3f-87b9-825674709757",
         "logProfileId": "",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 3,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-24T09:45:31Z" 
      },
      {
-        "logId": "",
+        "logId": "3a88cb31-14d3-4dfc-9888-c9092f8db829",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 5,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-25T11:15:43Z" 
      },
      {
-        "logId": "",
+        "logId": "226b1182-b7f8-47cf-adca-9638b6263f4f",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 4,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-25T11:15:52Z" 
      },
      {
-        "logId": "",
+        "logId": "74bc001e-b74b-45ae-babb-08cf7803f067",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 3,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-26T08:08:39Z" 
      },
      {
-        "logId": "",
+        "logId": "d1d5b8dd-72e9-4869-93a4-db7cd4bb78fb",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 3,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-26T08:08:46Z" 
      },
      {
-        "logId": "",
+        "logId": "aaa2dc24-0ab9-4bed-bb95-b20a1f6c74b9",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 1,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-27T09:02:56Z" 
      },
      {
-        "logId": "",
+        "logId": "469748ee-1b45-4ddf-8994-72a0c05b10b6",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 3,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-27T09:02:59Z" 
      },
      {
-        "logId": "",
+        "logId": "071d9105-f712-40d4-957e-eb32cac5db53",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
         "logAnswer": 5,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-28T09:57:12Z" 
      },
      {
-        "logId": "",
+        "logId": "a02fe7b1-41e4-4037-aff4-a5208b967abe",
         "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
         "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
         "logAnswer": 3,
-        "logDatetime": "" 
+        "logDatetime": "2024-01-28T09:57:25Z" 
      }
 ]
 
 let journalData = [
     {
-        "journalId": "",
+        "journalId": "ff301da9-9d7a-49fd-af9c-865c98358e7c",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "024-01-22T09:10:00Z",
         "journalText": "I was living in the moment all day, and it kept getting me killed by Nazis. I think you need to think ahead and live in the moment.",
         "journalTitle": "What is this? Full House?"
     },
     {
-        "journalId": "",
+        "journalId": "00695895-3862-4a0f-9683-bc4986871f9f",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-23T10:23:00Z",
         "journalText": "Stirred by starlight citizens of distant epochs a still more glorious dawn awaits are creatures of the cosmos concept of the number one a still more glorious dawn awaits.",
         "journalTitle": "Shumshumschilpiddydah!"
     },
     {
-        "journalId": "",
+        "journalId": "af697fc2-0627-483e-93da-302c525d5f62",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-24T10:04:11Z",
         "journalText": "After his parents' tragic death in a railroad accident, he gained the power to summon ghost trains. It's not all bad though, they were spared having to see their grown son wear a whistle.",
         "journalTitle": "Drake Equation"
     },
     {
-        "journalId": "",
+        "journalId": "48606cb6-50c2-42a1-9af0-c29fe64e946d",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-25T17:34:43Z",
         "journalText": "Inconspicuous motes of rock and gas encyclopaedia galactica star stuff harvesting star light the only home we've ever known bits of moving fluff the sky calls to us.",
         "journalTitle": "Copilot Disperser"
     },
     {
-        "journalId": "",
+        "journalId": "3d8d987b-96b6-4033-82d8-321afbeddcd5",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-26T08:08:39Z",
         "journalText": "Orion's sword inconspicuous motes of rock and gas network of wormholes vastness is bearable only through love a mote of dust suspended in a sunbeam science? ",
         "journalTitle": "I'm Scary Terry!!"
     },
     {
-        "journalId": "",
+        "journalId": "6866db3a-ca72-4cf4-96a6-cefd5eae87bb",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-27T18:26:44Z",
         "journalText": "Stirred by starlight citizens of distant epochs a still more glorious dawn awaits are creatures of the cosmos concept of the number one a still more glorious dawn awaits.",
         "journalTitle": "Apollonius of Perga"
     },
     {
-        "journalId": "",
+        "journalId": "3d552aa3-3bff-4da9-96d8-0e3478fbb617",
         "journalProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "journalDateTime": "",
+        "journalDateTime": "2024-01-28T12:33:22Z",
         "journalText": "The ash of stellar alchemy hydrogen atoms paroxysm of global death Sea of Tranquility are creatures of the cosmos two ghostly white figures in coveralls and helmets are softly dancing and billions upon billions upon billions upon billions upon billions upon billions upon billions.",
         "journalTitle": "Fuck you god, not today bitch!"
     }
