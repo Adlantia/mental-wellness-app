f972cd3ac1d0173309b21047e01364e13c2b81ab
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-12 12:01:29 -0600

    Adding to profile model

---
+++ frontend/src/utils/models/profile.model.ts
@@ -0,0 +1,39 @@
+import { z } from 'zod'
+
+export const ProfileSchema = z.object({
+    profileId: z.string({
+        required_error: 'profileId is required',
+        invalid_type_error: 'Please provide a valid profileId'
+    })
+        .uuid({message: 'please provide a valid profileId'}),
+
+    profileEmail: z.string({
+        required_error: 'profile email is a required field.',
+        invalid_type_error: 'please provide a valid profile email'
+    })
+        .email({message: 'please provide a valid email'})
+        .max(128, {message: 'profile email length is too long'}),
+
+
+    profileHash: z.string({
+        required_error: 'profileHash is required',
+        invalid_type_error: 'please provide a valid profileHash'
+    })
+        .length(97, {message: 'profile hash must be 97 characters'}),
+
+    profileName: z.string({
+        required_error: 'profile name is required',
+        invalid_type_error: 'please provide a profile name'
+    })
+        .trim()
+        .min(1, {message: 'please provide a valid profile name (min 1 characters)'})
+        .max(32, {message: 'please provide a valid profile name (max 32 characters)'})
+})
+
+export const SignUpSchema = ProfileSchema
+    .extend({
+        profileName: z
+            .string({required_error: "Profile Name is required"})
+            .min(1, {message: 'please provide a valid profile name (min 1 characters'})
+            .max(32, {message: 'please provide a valid profile name (max 32 characters'})
+    })
