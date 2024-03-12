a2885ae481501094435c34e8f12e9ede3a0427eb
ash1shkhatri <akhatri1@cnm.edu>
2024-03-12 15:34:48 -0600

    fixed merge conflict on next.config file

--- frontend/src/app/log/[trackerId]/page.tsx
+++ frontend/src/app/log/[trackerId]/page.tsx
@@ -1,14 +1,26 @@
-'use client'
+'use server'
+
 import {RatingScale} from "@/app/RatingScale";
 import {SubmitButton} from "@/app/SubmitButton";
-import {useParams} from "next/navigation";
-import LoggingPage from "@/app/log/[trackerId]/LoggingPage";
+import {Tracker} from "@/utils/models/tracker.model";
+import { fetchTrackerByTrackerId} from "@/utils/http/tracker.http";
 
 
-export default function Tracker() {
-     const {trackerId} = useParams<{ trackerId: string }>()
-  //   console.log(params)
+export default async function Page({params}: {params: {trackerId: string}})  {
+            const {trackerId}= params
+
+            const tracker = await getData(trackerId)
     return (
-            <LoggingPage trackerId={trackerId}/>
+            <section className = 'container'>
+                <h1 className='text-3xl max-w-fit mx-auto py-4 mt-4'> {tracker.trackerQuestion} </h1>
+                <RatingScale />
+                <div className = 'flex justify-center gap-4 my-6'>
+                    <SubmitButton buttonName = 'Submit' />
+                    <SubmitButton buttonName = 'Skip' />
+                </div>
+            </section>
     )
+}
+async function getData(trackerId:string): Promise<Tracker> {
+    return  await fetchTrackerByTrackerId(trackerId)
 }
