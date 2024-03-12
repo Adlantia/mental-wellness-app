d0932107070c899004321fe59122f8d78d5e6965
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-12 11:33:08 -0600

    Started signup form

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import {Formik, FormikHelpers} from "formik";
+import {Formik, FormikHelpers, FormikProps} from "formik";
 import {toFormikValidationSchema} from "zod-formik-adapter";
 
 
@@ -16,7 +16,7 @@ export function SignUpForm() {
 
     const handleSubmit = (values: signUp, actions: FormikHelpers<SignUp>) => {
         const {setStatus, resetForm} = actions
-        fetch('/apis/sign-up', {
+        fetch('/apis/signup', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -46,7 +46,19 @@ export function SignUpForm() {
     )
 }
 
-export default function Signup() {
+export default function Signup(props: FormikProps<SignUp>) {
+
+    const {
+        status,
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        handleReset
+
+    } = props;
 
    return (
        <>
@@ -57,36 +69,58 @@ export default function Signup() {
                      <p className="mb-4">Create your account. It's free and only takes a minute</p>
                         <hr className="mt-3"></hr>
 
-                   <form>
-                       <div className="grid grid-cols-2 gap-4">
+                   <form onSubmit={handleSubmit}>
                            <div className="flex justify-between">
                                <label htmlFor="First name"></label>
-                               <input type="text" className="border border-gray-400 py-1 w-40 rounded" placeholder="First name"/>
-                           </div>
-                           <div className="flex">
-                               <label htmlFor="Last name"></label>
-                               <input type="text" className="border border-gray-400 py-1 w-40 rounded" placeholder="Last name"/>
-                           </div>
+                               <input placeholder="First name"
+                                      onBlur={handleBlur}
+                                      onChange={handleChange}
+                                      // value={values.profileName}
+                                      className="border border-gray-400 py-1 w-full rounded"
+                                      type="text"
+                                      name="firstName"
+                                      id="firstName"
+                               />
+                               <DisplayError errors={errors} touched={touched} field={"profileName"} />
                        </div>
-                         <div className="mt-5 ">
+
+                         <div className="mt-5">
                              <label htmlFor="email"></label>
-                             <input type="email" className="border border-gray-400 py-1 px-2 w-full rounded" placeholder="Email" required/>
+                             <input placeholder="Email" required
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    // value={values.profileEmail}
+                                    className="border border-gray-400 py-1 px-2 w-full rounded"
+                                    type="text"
+                                    name="profileEmail"
+                                    id="profileEmail"
+                             />
+                             <DisplayError errors={errors} touched={touched} field={"profileEmail"} />
                          </div>
+
+
                         <div className="mt-5 ">
                             <label htmlFor="password"></label>
                             <input type="password" className="border border-gray-400 py-1 px-2 w-full rounded"
-                                                     placeholder="Password" required/>
+                                                     placeholder="Password" required
+                                   onBlur={handleBlur}
+                                   onChange={handleChange}
+
+
+                            />
                          </div>
+
+
                         <div className="mt-5 ">
                            <label htmlFor="confirm password"></label>
                            <input type="password" className="border border-gray-400 py-1 px-2 w-full rounded" placeholder="Confirm Password" required/>
                         </div>
                         <div className="mt-5">
                            <label htmlFor="checkbox"></label>
-                           <input type="checkbox" className="border border-gray-400 rounded"/>
-                             <span className="pl-2">
-                               I accept the <a href="#" className="text-purple-500 font-semibold">Terms of Use</a>
-                             </span>
+                           {/*<input type="checkbox" className="border border-gray-400 rounded"/>*/}
+                           {/*  <span className="pl-2">*/}
+                           {/*    I accept the <a href="#" className="text-purple-500 font-semibold">Terms of Use</a>*/}
+                           {/*  </span>*/}
                         </div>
                        <div className="mt-5">
                            <button className="border-indigo-700 bg-indigo-700 text-white w-full py-3 rounded-md hover:bg-transparent hover:text-indigo-700 font-semi-bold">Register</button>
