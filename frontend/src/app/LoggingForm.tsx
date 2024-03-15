ee354dee84ff5a634f4d5f00698ca7337d1e8ed9
DMCatanach <dawn.catanach@gmail.com>
2024-03-14 22:43:50 -0600

    Puts the code for handleSubmit and checking the session in place, with comments for code that still needs to be written

--- frontend/src/app/LoggingForm.tsx
+++ frontend/src/app/LoggingForm.tsx
@@ -1,13 +1,52 @@
+'use client'
 import {RatingScale} from "@/app/RatingScale";
 import {SubmitButton} from "@/app/SubmitButton";
+import {Formik, FormikHelpers} from "formik";
+import { Session } from "@/utils/models/fetchSession";
+import {useRouter} from "next/navigation";
+import {json} from "node:stream/consumers";
 
-export function LoggingForm() {
+type LoggingFormProps = {
+    session: Session | undefined
+}
+
+export function LoggingForm(props: LoggingFormProps) {
+    const router = useRouter()
+    const {session} = props
+    if (session === undefined) {
+        return <></>
+    }
+    const {profile, authorization} = session
+    const  initialValues = {
+        logAnswer = ''
+    }
+    // here there be formSchema, related to LogSchema, which I have not yet made
+    // here there be type for Values, which is dependent on formSchema
+    const handleSubmit = (values: Values, actions: FormikHelpers<any>) => {
+        let log = {logAnswer: values.logAnswer}
+        let {setStatus, resetForm} = actions
+        fetch('apis/log', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "authorization": `${authorization}`
+            },
+            body: JSON.stringify(log) //or should we be passing ratingOption here?
+        }).then(response => response.json()).then(json => {
+            if (json.status === 200) {
+                resetForm()
+                router.refresh()
+            }
+            setStatus({type: json, message: json.message})
+        })
+    }
+
+    // Does the Formik tag wrap the RatingScale and submit button in the following return?
     return (
         <>
         <RatingScale />
         <div className = 'flex justify-center gap-4 my-6'>
             <SubmitButton buttonName = 'Submit' />
-                    {/* <SubmitButton buttonName = 'Skip' /> */}
         </div>
     </>
     )
