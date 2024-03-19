5cd4a19d6e1976b8674ecd31895344854a6cbc77
DMCatanach <dawn.catanach@gmail.com>
2024-03-19 09:23:52 -0600

    Restores session props trying to get handleChange to work; now the form doesn't even show up

--- frontend/src/app/LoggingForm.tsx
+++ frontend/src/app/LoggingForm.tsx
@@ -1,10 +1,9 @@
 'use client'
-import {Field, Form, Formik, FormikHelpers} from "formik";
+import {Form, Formik, FormikHelpers} from "formik";
 import {Session} from "@/utils/models/fetchSession";
 import {useRouter} from "next/navigation";
-import {Values} from "zod";
+import {Values, z} from "zod";
 import {LogSchema} from "@/utils/models/log.model";
-
 
 type LoggingFormProps = {
     session: Session | undefined
@@ -12,28 +11,26 @@ type LoggingFormProps = {
 
 export function LoggingForm(props: LoggingFormProps) {
     const router = useRouter()
-    //TODO: put the next five lines back when session issue sorted; solution found, need to find where to implement
-    // const {session} = props
-    // if (session === undefined) {
-    //     return <></>
-    // }
-    // const {profile, authorization} = session
+    const {session} = props
+    if (session === undefined) {
+        return <></>
+    }
+    const {profile, authorization} = session
     const initialValues = {
-        logAnswer: ''
+        logAnswer: 0
     }
     const formSchema = LogSchema.pick({logAnswer: true})
     type Values = z.infer<typeof formSchema>
     const handleSubmit = (values: Values, actions: FormikHelpers<any>) => {
         let log = {"logProfileId": profile.profileId, "logId": null, "logTrackerId": null, "logAnswer": values.logAnswer, "logDatetime": null}
         let {setStatus, resetForm} = actions
-        console.log(log)
         fetch('apis/log', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                // "authorization": `${authorization}`
+                "authorization": `${authorization}`
             },
-            body: JSON.stringify(log) //or should we be passing ratingOption here?
+            body: JSON.stringify(log)
         }).then(response => response.json()).then(json => {
             if (json.status === 200) {
                 resetForm()
@@ -43,17 +40,19 @@ export function LoggingForm(props: LoggingFormProps) {
         })
     }
 
+    const {handleChange} = props
+
     return (
         <>
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
                 <div>
                     <Form>
                         <div className='flex justify-center my-6 gap-4' role={"group"}>
-                            <label className='btn btn-circle focus:bg-slate-400'><Field type="radio" name={"rating"} value={'1'}/>1</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><Field type="radio" name={"rating"} value={'2'}/>2</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><Field type="radio" name={"rating"} value={'3'}/>3</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><Field type="radio" name={"rating"} value={'4'}/>4</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><Field type="radio" name={"rating"} value={'5'}/>5</label>
+                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={1} onChange={handleChange}/>1</label>
+                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={2} onChange={handleChange}/>2</label>
+                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={3} onChange={handleChange}/>3</label>
+                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={4} onChange={handleChange}/>4</label>
+                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={5} onChange={handleChange}/>5</label>
                         </div>
                     </Form>
                     <div className='flex justify-center gap-4 my-6'>
