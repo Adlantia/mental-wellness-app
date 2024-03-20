ded81661f0597d6e7f2be59a648d106b49e490a5
DMCatanach <dawn.catanach@gmail.com>
2024-03-20 08:50:53 -0600

    Logging form works

--- frontend/src/app/LoggingForm.tsx
+++ frontend/src/app/LoggingForm.tsx
@@ -5,6 +5,7 @@ import {useRouter} from "next/navigation";
 import {Values, z} from "zod";
 import {LogSchema} from "@/utils/models/log.model";
 import {FormDebugger} from "@/app/components/formDebugger";
+import {DisplayError} from "@/app/components/displayError";
 
 type LoggingFormProps = {
     session: Session,
@@ -26,9 +27,16 @@ export function LoggingForm(props: LoggingFormProps) {
     const formSchema = LogSchema.pick({logAnswer: true})
     type Values = z.infer<typeof formSchema>
     const handleSubmit = (values: Values, actions: FormikHelpers<any>) => {
-        let log = {"logProfileId": profile.profileId, "logId": null, "logTrackerId": trackerId, "logAnswer": values.logAnswer, "logDatetime": null}
+        let log = {
+            "logProfileId": profile.profileId,
+            "logId": null,
+            "logTrackerId": trackerId,
+            "logAnswer": Number(values.logAnswer),
+            "logDatetime": null
+        }
+        console.log(log)
         let {setStatus, resetForm} = actions
-        fetch('apis/log', {
+        fetch('/apis/log', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -44,28 +52,34 @@ export function LoggingForm(props: LoggingFormProps) {
         })
     }
 
-    const {handleChange} = props
-
     return (
         <>
             <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-                <div>
-                    <Form>
-                        <div className='flex justify-center my-6 gap-4' role={"group"}>
-                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={1} onChange={handleChange}/>1</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={2} onChange={handleChange}/>2</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={3} onChange={handleChange}/>3</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={4} onChange={handleChange}/>4</label>
-                            <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={5} onChange={handleChange}/>5</label>
-                        </div>
-
-                    <div className='flex justify-center gap-4 my-6'>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </div>
-                    </Form>
-                    <FormDebugger {...props} />
-                </div>
+                {LoggingFormContent}
             </Formik>
+        </>
+    )
+}
+
+function LoggingFormContent(props: any) {
+    const {handleChange, handleSubmit, errors, touched} = props;
+
+    return (
+        <>
+            <Form onSubmit={handleSubmit}>
+                <div className='flex justify-center my-6 gap-4' role={"group"}>
+                    <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={1} onChange={handleChange}/>1</label>
+                    <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={2} onChange={handleChange}/>2</label>
+                    <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={3} onChange={handleChange}/>3</label>
+                    <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={4} onChange={handleChange}/>4</label>
+                    <label className='btn btn-circle focus:bg-slate-400'><input type="radio" name={"logAnswer"} value={5} onChange={handleChange}/>5</label>
+                </div>
+                <DisplayError errors={errors} touched={touched} field={"logAnswer"} />
+                <div className='flex justify-center gap-4 my-6'>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </div>
+            </Form>
+            <FormDebugger {...props} />
         </>
     )
 }
