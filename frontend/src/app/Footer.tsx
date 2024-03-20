ab6e20a15ce5cd95fe0bf9e34cf8b4c7e75a14c3
Adelita Martinez <amartinez1013@cnm.edu>
2024-03-20 10:56:20 -0600

    fixed footer link

--- frontend/src/app/Footer.tsx
+++ frontend/src/app/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer () {
     return (
         <footer className="footer footer-center p-4 bg-base-300 text-base-content">
             <aside>
-                <a className="hover:text-sky-500 pl-8 hover:underline" href="https://github.com/Adlantia/mental-wellness-app">Want to see the code behind Mindsight? Click here.</a>
+                <a className="hover:text-sky-500 pl-8 hover:underline" href="https://github.com/Adlantia/mental-wellness-app" target="_blank">Want to see the code behind Mindsight? Click here.</a>
             </aside>
         </footer>
     )
