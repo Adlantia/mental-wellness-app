987b2e5633c421f2bd257f2a5303150cb0304540
DMCatanach <dawn.catanach@gmail.com>
2024-03-20 11:10:37 -0600

    Updates seedData.js with Todos and copy/paste objects for populating log data

--- seed-data/seedData.js
+++ seed-data/seedData.js
@@ -1,114 +1,118 @@
-let trackerData = [
-    {
-        "trackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
-        "trackerCategory": "mood", 
-        "trackerQuestion": "How are you feeling today?"
-    },
-    {
-        "trackerId": "de223303-bab6-4217-a043-97030e3195b0",
-        "trackerCategory": "sleep",
-        "trackerQuestion": "How well did you sleep?"
-    }
-]
 
+// TODO Use these to make log requests in Insomnia, alternating mood and sleep trackerId
+// TODO Paste in your profile uuid for logProfileId
 let logData = [
+    //mood
     {
-       "logId": "20f013f3-e541-4a7f-936f-f5b224a63c76",
-       "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-       "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+       "logId": null,
+       "logProfileId": "",
+       "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
        "logAnswer": 4,
-       "logDatetime": "2024-01-22T09:00:00Z" 
+       "logDatetime": null
     },
+    //sleep
     {
-        "logId": "8ece0cfe-7e89-4e25-a3e2-ae4b67c56970",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
-        "logAnswer": 2,
-        "logDatetime": "2024-01-22T09:05:00Z" 
-     },
-     {
-        "logId": "8de17e3a-b7b2-4b81-8516-67249173df6b",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
-        "logAnswer": 2,
-        "logDatetime": "2024-01-23T10:00:00Z" 
-     },
-     {
-        "logId": "75b549c2-9a90-466a-9114-8f0355bc926a",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
-        "logAnswer": 5,
-        "logDatetime": "2024-01-23T10:00:10Z" 
-     },
-     {
-        "logId": "a3efcf4a-525d-400c-ad37-324ece7f37ec",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
-        "logAnswer": 2,
-        "logDatetime": "2024-01-24T09:45:00Z" 
-     },
-     {
-        "logId": "c631efc6-9438-4c3f-87b9-825674709757",
+        "logId": null,
         "logProfileId": "",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
-        "logAnswer": 3,
-        "logDatetime": "2024-01-24T09:45:31Z" 
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
+        "logAnswer": 2,
+        "logDatetime": null
      },
+    //mood
      {
-        "logId": "3a88cb31-14d3-4dfc-9888-c9092f8db829",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
+        "logAnswer": 2,
+        "logDatetime": null
+     },
+    //sleep
+     {
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
         "logAnswer": 5,
-        "logDatetime": "2024-01-25T11:15:43Z" 
+        "logDatetime": null
      },
+    //mood
      {
-        "logId": "226b1182-b7f8-47cf-adca-9638b6263f4f",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
+        "logAnswer": 2,
+        "logDatetime": null
+     },
+    //sleep
+     {
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2", //sleep
+        "logAnswer": 3,
+        "logDatetime": null
+     },
+    //mood
+     {
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
+        "logAnswer": 5,
+        "logDatetime": null
+     },
+    //sleep
+     {
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
         "logAnswer": 4,
-        "logDatetime": "2024-01-25T11:15:52Z" 
+        "logDatetime": null
      },
+    //mood
      {
-        "logId": "74bc001e-b74b-45ae-babb-08cf7803f067",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
         "logAnswer": 3,
-        "logDatetime": "2024-01-26T08:08:39Z" 
+        "logDatetime": null
      },
+    //sleep
      {
-        "logId": "d1d5b8dd-72e9-4869-93a4-db7cd4bb78fb",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
         "logAnswer": 3,
-        "logDatetime": "2024-01-26T08:08:46Z" 
+        "logDatetime": null
      },
+    //mood
      {
-        "logId": "aaa2dc24-0ab9-4bed-bb95-b20a1f6c74b9",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
         "logAnswer": 1,
-        "logDatetime": "2024-01-27T09:02:56Z" 
+        "logDatetime": null
      },
+    //sleep
      {
-        "logId": "469748ee-1b45-4ddf-8994-72a0c05b10b6",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
         "logAnswer": 3,
-        "logDatetime": "2024-01-27T09:02:59Z" 
+        "logDatetime": null
      },
+    //mood
      {
-        "logId": "071d9105-f712-40d4-957e-eb32cac5db53",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "4ccace62-2947-4935-8bab-99e4606c606a",
         "logAnswer": 5,
-        "logDatetime": "2024-01-28T09:57:12Z" 
+        "logDatetime": null
      },
+    //sleep
      {
-        "logId": "a02fe7b1-41e4-4037-aff4-a5208b967abe",
-        "logProfileId": "cc1edac6-0eee-43aa-a859-51aa030874ce",
-        "logTrackerId": "de223303-bab6-4217-a043-97030e3195b0",
+        "logId": null,
+        "logProfileId": "",
+        "logTrackerId": "978ebe61-4073-4cd6-a733-48410083e5b2",
         "logAnswer": 3,
-        "logDatetime": "2024-01-28T09:57:25Z" 
+        "logDatetime": null
      }
 ]
 
@@ -163,3 +167,16 @@ let journalData = [
         "journalTitle": "Fuck you god, not today bitch!"
     }
 ]
+
+// let trackerData = [
+//     {
+//         "trackerId": "00bf1e98-7212-4c48-a044-b11601a68c17",
+//         "trackerCategory": "mood",
+//         "trackerQuestion": "How are you feeling today?"
+//     },
+//     {
+//         "trackerId": "de223303-bab6-4217-a043-97030e3195b0",
+//         "trackerCategory": "sleep",
+//         "trackerQuestion": "How well did you sleep?"
+//     }
+// ]
